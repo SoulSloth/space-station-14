7a30e3492dc45950087dbaa02569b29da2f28a36
SoulSloth <root@DESKTOP-EKBQMB8.localdomain>
2020-09-07 09:28:06 -0400

    added DefibbComponent

---
+++ Content.Server/GameObjects/Components/Medical/DefibComponent.cs
@@ -0,0 +1,18 @@
+﻿using Content.Shared.Interfaces.GameObjects.Components;
+using Robust.Server.Interfaces.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Content.Server.GameObjects.Components.Medical
+{
+    [RegisterComponent]
+    public class DefibComponent: Component, IUse
+    {
+        [Dependency] private readonly IServerEntityManager _serverEntityManager = default!;
+        public override string Name => "Defib";
+        public bool UseEntity(UseEntityEventArgs eventArgs)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
