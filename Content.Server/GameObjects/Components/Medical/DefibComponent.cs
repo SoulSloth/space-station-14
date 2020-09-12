a43e60b26b202b4166bab81a7b17696396cdc8e6
SoulSloth <root@DESKTOP-EKBQMB8.localdomain>
2020-09-12 10:09:40 -0400

    change defibrillator into a backpack. Added defib paddles

--- Content.Server/GameObjects/Components/Medical/DefibComponent.cs
+++ Content.Server/GameObjects/Components/Medical/DefibComponent.cs
@@ -1,18 +1,59 @@
-﻿using Content.Shared.Interfaces.GameObjects.Components;
+﻿using System.Linq;
+using Content.Server.GameObjects.Components.Items.Storage;
+using Content.Server.Interfaces.GameObjects.Components.Items;
+using Content.Shared.Interfaces.GameObjects.Components;
+using Robust.Server.GameObjects.Components.Container;
 using Robust.Server.Interfaces.GameObjects;
 using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
 using Robust.Shared.IoC;
+using Robust.Shared.Map;
 
 namespace Content.Server.GameObjects.Components.Medical
 {
     [RegisterComponent]
-    public class DefibComponent: Component, IUse
+    public class DefibComponent : Component, IUse, IDropped
     {
         [Dependency] private readonly IServerEntityManager _serverEntityManager = default!;
         public override string Name => "Defib";
+
+        public ContainerSlot paddleContianer = default!;
+
+        public IEntity defibPaddles = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            paddleContianer = ContainerManagerComponent.Ensure<ContainerSlot>($"{Name}-paddleContainer", Owner);
+            paddleContianer.Insert(
+                    _serverEntityManager.SpawnEntity("DefibrillatorPaddles",Owner.Transform.Coordinates));
+            paddleContianer.ContainedEntity.GetComponent<DefibPaddles>().defib = Owner;
+        }
+
         public bool UseEntity(UseEntityEventArgs eventArgs)
         {
-            throw new System.NotImplementedException();
+
+            //TODO: No paddle message
+            if (paddleContianer.ContainedEntity == null) return false;
+
+            var hands = eventArgs.User.GetComponent<IHandsComponent>();
+            var firstInactiveHand = hands.GetFirstInactiveEmptyHand();
+
+            //TODO: Hands are full message.
+            if (firstInactiveHand == null) return false;
+
+            var paddles = paddleContianer.ContainedEntity;
+            hands.PutInHand(paddles.GetComponent<ItemComponent>(), firstInactiveHand!, false);
+
+            defibPaddles = paddles;
+
+
+            return true;
+        }
+
+        public void Dropped(DroppedEventArgs eventArgs)
+        {
+            if (defibPaddles != null) paddleContianer.Insert(defibPaddles);
         }
     }
 }
---
+++ Content.Server/GameObjects/Components/Medical/DefibPaddles.cs
@@ -0,0 +1,20 @@
+﻿using Content.Shared.Interfaces.GameObjects.Components;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Interfaces.GameObjects;
+
+namespace Content.Server.GameObjects.Components.Medical
+{
+    [RegisterComponent]
+    public class DefibPaddles: Component, IDropped
+    {
+        public override string Name => "DefibPaddles";
+
+        public IEntity defib;
+
+        public void Dropped(DroppedEventArgs eventArgs)
+        {
+            var myOwner = defib.GetComponent<DefibComponent>();
+            myOwner.paddleContianer.Insert(Owner);
+        }
+    }
+}
